6105ef8101687cc09f2d2c112ae8777c972fd616
weixintao <weixintao@win7.com>
2016-01-28 20:20:08 +0800

    android action

--- src/actions/android/android_solution.lua
+++ src/actions/android/android_solution.lua
@@ -1,23 +1,49 @@
+-- Function to make sure that an option in a given config is the same for every project
+-- Additionnaly, replace "default" with "nil"
+local function agregateOption(sln, cfg, option)
+  local first = true
+  local val
+  for prj in premake.solution.eachproject(sln) do
+    for prjcfg in premake.eachconfig(prj) do
+      if prjcfg.shortname == cfg.shortname then
+        if first then
+          first = false
+          val = prjcfg[option]
+        else
+          if prjcfg[option] ~= val then
+            error("Value for "..option.." must be the same on every project for configuration "..cfg.longname.." in solution "..sln.name)
+          end
+        end
+      end
+    end
+  end
+  if val == "default" then
+    return nil
+  end
+  return val
+end
+
+
 
 function premake.android.applicationmk(sln)
-	for cfg in solution.eachconfig(sln) do
+    local sln = premake.solution.get(1)
+	if sln ~= nil then
+		if sln.ndkabi ~= nil then
+			_p('  APP_ABI := %s', sln.ndkabi)
+		end
+		if sln.ndkplatform ~= nil then
+			_p('  APP_PLATFORM := %s', sln.ndkplatform)
+		end
+		if sln.ndktoolchainversion ~= nil then
+			_p('  NDK_TOOLCHAIN_VERSION := %s', sln.ndktoolchainversion)
+		end
+		if sln.ndkstl ~= nil then
+			_p('  APP_STL := %s', sln.ndkstl)
+		end
 
-		if cfg.ndkabi ~= nil then
-			_p('  APP_ABI := %s', cfg.ndkabi)
-		end
-		if cfg.ndkplatform ~= nil then
-			_p('  APP_PLATFORM := %s', cfg.ndkplatform)
-		end
-		if cfg.ndktoolchainversion ~= nil then
-			_p('  NDK_TOOLCHAIN_VERSION := %s', cfg.ndktoolchainversion)
-		end
-		if cfg.ndkstl ~= nil then
-			_p('  APP_STL := %s', cfg.ndkstl)
-		end
-
-		if cfg.optimize == p.OFF or cfg.optimize == "Debug" then
+		if sln.optimize == p.OFF or sln.optimize == "Debug" then
 			_p('  APP_OPTIM := debug')
-		elseif optim ~= nil then
+		elseif sln.optim ~= nil then
 			_p('  APP_OPTIM := release')
 		end
 	end
